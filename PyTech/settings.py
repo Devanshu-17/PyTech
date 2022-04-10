5e4d30bc712c23e1362d0bb7dabed37239244970
Devanshu-17 <93381397+Devanshu-17@users.noreply.github.com>
2022-04-11 01:45:29 +0530

    Update settings.py

--- PyTech/settings.py
+++ PyTech/settings.py
@@ -152,8 +152,8 @@ MESSAGE_TAGS = {
 # SMTP configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'pytech.shop@gmail.com'
-EMAIL_HOST_PASSWORD = 'qsaw1234Q@'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 #Activate Django-Heroku
